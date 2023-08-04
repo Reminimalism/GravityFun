61b5d2566c209a3c6f8cdf4eb44db093639e1ac2
Majidzadeh <hashpragmaonce@gmail.com>
2023-08-04 16:10:32 +0330

    Update version

--- GravityFun/Info.h
+++ GravityFun/Info.h
@@ -7,7 +7,7 @@ namespace GravityFun::Info
     /// @brief The name to print. No newline character.
     const char * const NAME = "Gravity Fun";
     /// @brief The version without 'v'. No newline character.
-    const char * const VERSION = "0.5-dev";
+    const char * const VERSION = "0.6";
 
     /// @brief Indented multiline string with extra newlines at the beginning and the end.
     const char * const DESCRIPTION = R"(
